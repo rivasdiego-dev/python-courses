9cdf10b715d1eb96e0570f7da2a35546c0b2e25e
Diego José Rivas <62678420+rivasdiego-dev@users.noreply.github.com>
2022-12-05 23:07:47 +0000

    Excercise 1 finished ✅

--- Python4E/05-Loops-Iterations/EX1.py
+++ Python4E/05-Loops-Iterations/EX1.py
@@ -1,8 +1,18 @@
 total_numbers = 0
+average = 0
+sum = 0
 
 while True:
   try:
-    list[total_numbers] = int(input('Enter a number'))
+    user_input = input('Enter a number ')
+    if user_input == 'done': break
+    user_input = int(user_input)
+    total_numbers += 1
+    sum += user_input
   except:
     print('Bad input')
-  
+    continue
+
+average = sum / total_numbers
+
+print('Numbers count: ',total_numbers, '\nTotal sum: ', sum, '\nAverage: ', average)
